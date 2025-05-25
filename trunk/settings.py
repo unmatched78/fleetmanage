704992599985f233ea741a826bebfc7e5749ce3e
unmatched78 <marvinavi24@gmail.com>
2025-05-25 22:58:29 +0200

    cleaning

--- trunk/settings.py
+++ trunk/settings.py
@@ -86,8 +86,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True  
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Your Gmail address  
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use an App Password for increased security  
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Gmail address  
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use an App Password
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 #cloudinary settings
