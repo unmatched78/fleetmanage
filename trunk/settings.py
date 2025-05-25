bd184d76ca3c955b3231fdb61790963c476224b6
unmatched78 <marvinavi24@gmail.com>
2025-05-25 23:23:20 +0200

    django secret key

--- trunk/settings.py
+++ trunk/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-bfvzl$hlv%l04$xg)c1c!iz5abe&(0_8e+-65%5mo%2^i**=v4"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
