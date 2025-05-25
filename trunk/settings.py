497d7db8f65e496c8bab34214fce8292996f3bf5
unmatched78 <marvinavi24@gmail.com>
2025-05-25 22:57:00 +0200

    cleaning

--- trunk/settings.py
+++ trunk/settings.py
@@ -9,11 +9,6 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-bfvzl$hlv%l04$xg)c1c!iz5abe&(0_8e+-65%5mo%2^i**=v4"
 
