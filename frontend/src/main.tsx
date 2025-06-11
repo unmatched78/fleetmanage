158ffd78601228017d424c54dc61542ce226784c
unmatched78 <marvinavi24@gmail.com>
2025-06-11 23:40:13 +0200

    for caching UI

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,21 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from "@/components/theme-provider"
 import { AuthProvider } from './context/AuthContext';
+import { Toaster } from "@/components/ui/sonner";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
+  <>
+    <Toaster />
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <QueryClientProvider client={queryClient}>
   <AuthProvider>
   <StrictMode>
     <App />
   </StrictMode>
   </AuthProvider>
+  </QueryClientProvider>
   </ThemeProvider>
+  </>
   ,
 )
